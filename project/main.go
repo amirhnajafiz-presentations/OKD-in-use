7395b3fcb39b3a4dad24202f0128212c0afe0ae7
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-13 15:34:25 +0330

    add: server handler

--- project/main.go
+++ project/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/amirhnajafiz-presentations/snapp-OKD-in-use/project/internal/db"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	req := new(db.Config)
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+
+		log.Println(fmt.Errorf("failed to parse request: %w", err))
+
+		return
+	}
+
+	err := db.Database{}.Connect(*req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
 
 }
