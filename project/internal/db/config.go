2519e85be56d98d9f84ec1d03bbfbcbf92e844af
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-13 15:28:31 +0330

    update: db config and db connect

--- project/internal/db/config.go
+++ project/internal/db/config.go
@@ -1,9 +1,15 @@
 package db
 
+import "fmt"
+
 type Config struct {
 	Host     string `json:"host"`
 	Port     int    `json:"port"`
 	User     string `json:"user"`
 	Password string `json:"password"`
 	Database string `json:"database"`
+}
+
+func (c Config) DNS() string {
+	return fmt.Sprintf("mysql://%s:%s@tcp(%s:%d)/%s", c.User, c.Password, c.Host, c.Port, c.Database)
 }
--- project/internal/db/database.go
+++ project/internal/db/database.go
@@ -1,11 +1,22 @@
 package db
 
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 type Database struct{}
 
-func (d Database) Connect() {
+func (d Database) Connect(cfg Config) error {
+	db, err := sql.Open("mysql", cfg.DNS())
+	if err != nil {
+		return err
+	}
 
+	return d.ping(db)
 }
 
-func (d Database) Ping() {
-
+func (d Database) ping(conn *sql.DB) error {
+	return conn.Ping()
 }
